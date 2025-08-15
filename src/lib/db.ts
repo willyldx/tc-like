3cfcdbdef87b58e94c1c245b368509496d32f55b
willyldx <spencer62@protonmail.com>
2025-08-15 02:59:51 +0100

    feat: add prisma db client

---
+++ src/lib/db.ts
@@ -0,0 +1,18 @@
+// src/lib/db.ts
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // évite de recréer le client en dev (hot-reload)
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
+export default prisma;
